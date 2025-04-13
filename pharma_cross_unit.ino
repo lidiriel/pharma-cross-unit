d62deb6241b80355369207580b35f8fa72b39608
ludovic <ludovic@scilink.net>
2025-04-13 08:07:54 +0200

    add first version of cross unit code

---
+++ pharma_cross_unit.ino
@@ -0,0 +1,102 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <ModbusRTUSlave.h>
+
+#define DEBUG false
+#define Serial if(DEBUG)Serial
+
+#define CROSS_B // define CROSS_A or CROSS_B
+
+#define pin_rs485_de 2
+#define pin_rx 3
+#define pin_tx 4
+SoftwareSerial my_serial(pin_rx, pin_tx);
+#define modbus_serial my_serial
+#define modbus_baud 38400
+#define modbus_config SERIAL_8N1
+
+#ifdef CROSS_A
+	#define modbus_unit_id 1
+	#define led_shift 0
+#else
+	#define modbus_unit_id 2
+	#define led_shift 8
+#endif
+
+const int pin_led_user = LED_BUILTIN;
+// output pin for controlling LEDs (4 blues, 4 green)
+const int pin_leds[8] = {5, 6, 7, 8, 9, 10, 11, 12};
+
+const int pin_switch = 13;
+
+#define register_led 0 // register used to change the state of the leds
+uint8_t led_state = 0;
+uint8_t new_state = 0;
+
+const uint8_t numHoldingRegisters = 1;
+ModbusRTUSlave modbus(modbus_serial, pin_rs485_de);
+uint16_t holdingRegisters[numHoldingRegisters];
+
+
+
+void setup()
+{
+	pinMode(pin_switch, INPUT_PULLUP);
+	Serial.begin(9600);
+	Serial.println("start setup");
+	// configure and check outputs LEDs
+	for(int i=0; i<8; i++){
+		int pin = pin_leds[i];
+		pinMode(pin, OUTPUT);
+		digitalWrite(pin, HIGH);
+		delay(250);
+		digitalWrite(pin, LOW);
+		delay(250);
+	}
+	modbus.configureHoldingRegisters(holdingRegisters, numHoldingRegisters);
+	modbus_serial.begin(modbus_baud);
+	modbus.begin(modbus_unit_id, modbus_baud, modbus_config);
+	Serial.println("end setup");
+}
+
+void update_leds(uint8_t new_state) {
+	if (new_state != led_state) {
+		led_state = new_state;
+		for (int i = 0; i < 8; i++) {
+			uint8_t value = (new_state & 1) ? HIGH : LOW;
+			digitalWrite(pin_leds[i], value);
+			new_state = new_state >> 1;
+		}
+		//Serial.println(reg);
+	}
+}
+
+int count_set_bits(unsigned int num)
+{
+    int count = 0;
+    while (num) {
+        count += num & 1; // Add the least significant bit
+        num >>= 1; // Right shift the number
+    }
+    return count;
+}
+
+void loop()
+{
+	if(digitalRead(pin_switch) == LOW){
+		// random mode
+		delay(500);
+		int count = 0;
+		do{
+			new_state = random(0, 255);
+			count = count_set_bits(new_state);
+		}while(count>5);
+		update_leds(new_state);
+	}else{
+		if(modbus.poll()){ // check message on modbus
+			uint16_t reg = holdingRegisters[register_led];
+			new_state = (uint8_t) (reg >> led_shift);
+			update_leds(new_state);
+		}
+	}
+}
---
+++ sloeber.ino.cpp
@@ -0,0 +1,20 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2025-04-12 20:10:08
+
+#include "Arduino.h"
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <ModbusRTUSlave.h>
+
+void setup() ;
+void update_leds(uint8_t new_state) ;
+int count_set_bits(unsigned int num) ;
+void loop() ;
+
+#include "pharma_cross_unit.ino"
+
+
+#endif
